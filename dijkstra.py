67421cd3ed09b8beec24274fcf093a2afc45a30f
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2013-10-12 20:32:06 +0800

    Generator-based Dijkstra shortest path search

---
+++ dijkstra.py
@@ -0,0 +1,77 @@
+from collections import namedtuple
+from heapq import heappush, heappop
+
+edge_cost = namedtuple('edge_cost', 'id cost')
+                
+def add_edge(graph, left, right, cost):
+    if left not in graph:
+        graph[left] = []
+    
+    graph[left].append(edge_cost(id = right, cost = cost))
+                
+def make_graph(source):
+    """
+    >>> make_graph([(1, 2, 10), (2, 3, 15), (1, 3, 30)])
+    {1: [edge_cost(id=2, cost=10), edge_cost(id=3, cost=30)], 2: [edge_cost(id=3, cost=15)]}
+    """
+    graph = {}
+    
+    for entry in source:
+        add_edge(graph, *entry)
+        
+    return graph
+    
+def reverse_graph(source):
+    """
+    >>> reverse_graph(make_graph([(1, 2, 10), (2, 3, 15), (1, 3, 30)]))
+    {2: [edge_cost(id=1, cost=10)], 3: [edge_cost(id=1, cost=30), edge_cost(id=2, cost=15)]}
+    """
+    reversed = {}
+    
+    for left, edges in source.items():
+        for right, cost in edges:
+            add_edge(reversed, right, left, cost)
+            
+    return reversed
+    
+def dijkstra_kernel(graph, start, end, previous):
+    queue = [(0.0, start)]
+    while queue:
+        cost, id = heappop(queue)
+        yield edge_cost(id = id, cost = cost)
+      
+        for neighbour in graph[id]:
+            alt_cost = cost + neighbour.cost
+            
+            # if there was no cost associated or cost is lower
+            if id not in previous or alt_cost < previous[neighbour.id].cost:
+                heappush(queue, (alt_cost, neighbour.id))
+                previous[neighbour.id] = edge_cost(id = id, cost = alt_cost)
+
+def backtrack(previous, id):
+    """
+    >>> list(backtrack({3: edge_cost(id=2, cost=15), 2: edge_cost(id=1, cost=10)}, 3))
+    [3, 2, 1]
+    """
+    yield id
+    while id in previous:
+        id = previous[id].id
+        yield id
+
+def dijkstra(graph, start, end):
+    """
+    >>> graph = make_graph([(1, 2, 10), (2, 3, 15), (1, 3, 30)])
+    >>> dijkstra(graph, 1, 3)
+    (25.0, [1, 2, 3])
+    """
+    previous = {}
+    for id, cost in dijkstra_kernel(graph, start, end, previous):
+        if id == end:
+            path = list(backtrack(previous, id))
+            path.reverse()
+            return (cost, path)
+            
+if __name__ == "__main__":
+    graph = make_graph([(1, 2, 10), (2, 3, 15), (1, 3, 30)])
+    dijkstra(graph, 1, 3)
+        
